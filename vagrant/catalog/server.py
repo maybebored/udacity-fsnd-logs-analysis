b1e5d45e428c4b0516501b91af8492912aaca673
maybebored <mayuselvaraja@gmail.com>
2019-04-07 17:24:27 +0800

    Added Google Sign  In

--- vagrant/catalog/server.py
+++ vagrant/catalog/server.py
@@ -1,42 +1,196 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, redirect, jsonify, url_for, flash
+from flask import session as login_session
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Product
 from datetime import datetime, date
+import random
+import string
+import json
+from flask import make_response
+import httplib2
+from oauth2client import client
+from oauth2client.client import FlowExchangeError
+import requests
 
 app = Flask(__name__)
 
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Mayuran's Product Catalog"
+
 # Connect to Database and create database session
-engine = create_engine('sqlite:///itemcatalog.db',connect_args={'check_same_thread': False})
+engine = create_engine('sqlite:///itemcatalog.db',
+                       connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-@app.route('/login', methods=['GET', 'POST'])
+# Check for logged in user; if not logged in return None
+def getLoggedInUser():
+    try:
+        return login_session['username']
+    except KeyError:
+        return None
+
+@app.route('/login')
 def showLogin():
-    return render_template('login.html')
+    user = getLoggedInUser()
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state, user=user)
+
+# (Receive auth_code by HTTPS POST)
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # If this request does not have `X-Requested-With` header, this could be a CSRF
+    if not request.headers.get('X-Requested-With'):
+        abort(403)
+
+    # Obtain authorization code
+    auth_code = request.data
+
+    # Set path to the Web application client_secret_*.json file you downloaded from the
+    # Google API Console: https://console.developers.google.com/apis/credentials
+    CLIENT_SECRET_FILE = 'client_secrets.json'
+    
+    try:
+        # Exchange auth code for access token, refresh token, and ID token
+        credentials = client.credentials_from_clientsecrets_and_code(CLIENT_SECRET_FILE,
+            ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email'], auth_code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+    # DISCONNECT - Revoke a current user's token and reset their login_session
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session['access_token']
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    if access_token is None:
+ 	print 'Access Token is None'
+    	response = make_response(json.dumps('Current user not connected.'), 401)
+    	response.headers['Content-Type'] = 'application/json'
+    	return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+	del login_session['access_token']
+    	del login_session['gplus_id']
+    	del login_session['username']
+    	del login_session['email']
+    	del login_session['picture']
+    	response = make_response(json.dumps('Successfully disconnected.'), 200)
+    	response.headers['Content-Type'] = 'application/json'
+    	return response
+    else:
+
+    	response = make_response(json.dumps(
+    	    'Failed to revoke token for given user.', 400))
+    	response.headers['Content-Type'] = 'application/json'
+    	return response
+
 
 @app.route('/')
-@app.route('/home/') #home page
+@app.route('/home/')  # home page
 def catalogMain():
-    products_query = session.query(Product).filter(Product.created_on > date(2019,1,1))
-    categories_query = session.query(Product.category.distinct().label("category"))
-    # products = [row.category for row in categories_query.all()]
-    categories = [ row.category for row in categories_query.all()]
-    return render_template('home.html',products=products_query,categories=categories)
+    user = getLoggedInUser()
+    products_query = session.query(Product).filter(
+        Product.created_on > date(2019, 1, 1))
+    categories_query = session.query(
+        Product.category.distinct().label("category"))
+    categories = [row.category for row in categories_query.all()]
+    return render_template('home.html', products=products_query, categories=categories, user=user)
 
-@app.route('/catalog/<string:category>/') #category page
+
+@app.route('/catalog/<string:category>/')  # category page
 def getProductsByCategory(category):
-    categories_query = session.query(Product.category.distinct().label("category"))
-    categories = [ row.category for row in categories_query.all()]
-    products_query = session.query(Product).filter(Product.category == category)
-    return render_template('category.html',products=products_query,categories=categories)
+    categories_query = session.query(
+        Product.category.distinct().label("category"))
+    categories = [row.category for row in categories_query.all()]
+    products_query = session.query(Product).filter(
+        Product.category == category)
+    return render_template('category.html', products=products_query, categories=categories)
+
 
 @app.route('/catalog/product/<int:product_id>/')
 def getProduct(product_id):
-    products_query = session.query(Product).filter(Product.id == product_id).one()
-    return render_template('product.html',product=products_query)
+    products_query = session.query(Product).filter(
+        Product.id == product_id).one()
+    return render_template('product.html', product=products_query)
 
 
 # JSON APIs for Product Catalog App
@@ -44,17 +198,18 @@ def getProduct(product_id):
 @app.route('/api/catalog/', methods=['GET'])
 def catalogJSON():
     products = session.query(Product).all()
-    return jsonify(catalog=[(p.title,p.description) for p in products]);
+    return jsonify(catalog=[(p.title, p.description) for p in products])
 
 # GET all categories
+
+
 @app.route('/api/catalog/categories/', methods=['GET'])
 def categoriesJSON():
     products = session.query(Product.category.distinct().label('category'))
-    return jsonify(categories = [p.category for p in products])
-
+    return jsonify(categories=[p.category for p in products])
 
 
 if __name__ == '__main__':
-    # app.secret_key = 'super_secret_key'
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
